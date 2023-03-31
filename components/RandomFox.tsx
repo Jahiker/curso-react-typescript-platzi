68fd6a0489c740855b9c84d0dafef04b4b819a6f
jahiker <rojasjahiker@gmail.com>
2023-03-30 21:32:20 -0400

    [add] readme

---
+++ components/RandomFox.tsx
@@ -0,0 +1,17 @@
+// import type { FunctionComponent, FC } from "react";
+
+export const RandomFox = (): JSX.Element => {
+    return <img src="" alt="" />;
+};
+
+// export const RandomFox1 = () => {
+//   return <img src="" alt="" />;
+// };
+
+// export const RandomFox2: FunctionComponent = () => {
+//   return <img src="" alt="" />;
+// };
+
+// export const RandomFox3: FC = () => {
+//   return <img src="" alt="" />;
+// };
